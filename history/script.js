944a4e9e284e535258cf286b9418cbb204305067
Cee <chaelsybenj@gmail.com>
2023-02-16 06:52:21 +0800

    del

--- history/script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {
-    var header = document.querySelector("header");
-    var sticky = header.offsetTop;
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
